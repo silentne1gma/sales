a2c3b573bfbe1d4f1f85806ba46a9418d08a9b8d
en1gma <silenten1gma@protonmail.com>
2021-03-20 13:34:46 +0100

    fixed

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ addBtn.addEventListener('click', ()=>{
         const numberOfSales = document.createElement("td");
         numberOfSales.setAttribute("id", "numberOfSales");
         // make a 2x btns + and - in the number sales div
-        numberOfSales.innerHTML = "<button id='sub'>-</button> <p id='number'></p> <button id='add'>+</button>" 
+        numberOfSales.innerHTML = "<button class='sub'>-</button> <p class='number'></p> <button class='add'>+</button>" 
         // make a td element that has the product name
         const tr = document.createElement('tr');
         table.appendChild(tr)
@@ -39,20 +39,23 @@ addBtn.addEventListener('click', ()=>{
         productName.value = "";
         modal[1].style.display = "none"
         // add event listner to add or sustract a product
-        const sub = document.getElementById('sub')
-        const add = document.getElementById('add')
-        const number = document.getElementById('number')     
-        number.innerText = 0
-        console.log(sub)
-        console.log(number)
-        sub.addEventListener('click', ()=>{
-            number.innerText-=1
-        })
-        add.addEventListener('click', ()=>{
-            console.log(number.innerText)
-            number.innerText = parseInt(number.innerText) + 1
-            console.log(number.innerText) 
-        })
+        const subs = document.getElementsByClassName('sub')
+        const adds = document.getElementsByClassName('add')
+        const numbers = document.getElementsByClassName('number')
+        console.log(numbers.length)
+        numbers[numbers.length-1].innerText = 0
+        for (let i = 0; i < numbers.length; i++) {
+            console.log(subs[i])
+            console.log(numbers[i])
+            subs[i].addEventListener('click', ()=>{
+                numbers[i].innerText-=1
+            })
+            adds[i].addEventListener('click', ()=>{
+                console.log(numbers[i].innerText)
+                numbers[i].innerText = parseInt(numbers[i].innerText) + 1
+                console.log(numbers[i].innerText) 
+            })
+        }
     }
 })
 
